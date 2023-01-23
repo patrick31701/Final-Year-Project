e32ff72b1cf70d8434cea4709d11b1217d681a46
Patrick Mc Elligott <patrickmcelligott2014@gmail.com>
2023-01-23 18:09:31 +0000

    Upload Base Code

---
+++ FinalYearProject/app/src/main/java/com/mad1/finalyearproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mad1.finalyearproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
