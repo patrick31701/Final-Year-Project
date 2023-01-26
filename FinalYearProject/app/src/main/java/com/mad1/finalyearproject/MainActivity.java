e410704d2e46d6b06ba2d348d6bd3d4d71cf4142
Patrick Mc Elligott <patrickmcelligott2014@gmail.com>
2023-01-26 21:25:35 +0000

    Basic Biometric read code

--- FinalYearProject/app/src/main/java/com/mad1/finalyearproject/MainActivity.java
+++ FinalYearProject/app/src/main/java/com/mad1/finalyearproject/MainActivity.java
@@ -1,14 +1,86 @@
 package com.mad1.finalyearproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText email, password;
+    Button login;
+    BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        login = findViewById(R.id.btn_login);
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Toast.makeText(getApplicationContext(), "App can authenticate using biometrics.", Toast.LENGTH_SHORT).show();
+                password.setEnabled(false);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(), "No biometric features available on this device.", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(getApplicationContext(), "Biometric features are currently unavailable.", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(getApplicationContext(), "The user hasn't associated any biometric credentials with their account.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        login.setOnClickListener(v -> {
+            String email = this.email.getText().toString();
+            String password = this.password.getText().toString();
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please enter all required details", Toast.LENGTH_SHORT).show();
+            } else {
+
+            }
+        });
+    }
+    public void biometricLogin()
+    {
+        Executor executor = ContextCompat.getMainExecutor(this);
+        androidx.biometric.BiometricPrompt biometricPrompt = new androidx.biometric.BiometricPrompt(MainActivity.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(), "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                Toast.makeText(getApplicationContext(), "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+        biometricPrompt.authenticate(promptInfo);
     }
 }
